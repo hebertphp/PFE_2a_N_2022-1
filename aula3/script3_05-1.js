080ef5bac9db37c1b4fc17252f6ed7cede44b6ff
Hebert <hebertphp@gmail.com>
2022-03-21 20:49:57 -0300

    PFE_2a_N

---
+++ aula3/script3_05-1.js
@@ -0,0 +1,18 @@
+const pi = 3.14;//declarando uma const
+var x , y;
+y = 3;//y=3
+y = 2;//y=2
+x = y * pi;//
+document.write ("x = "+ x);//
+/*
+Saída:
+x = 6.28
+
+y=20;
+y=19;
+y=6;
+y=10;
+y=0;
+document.write("y="+y);//y=
+
+*/
---
+++ aula3/script3_06-1.js
@@ -0,0 +1,4 @@
+var nome,ano,res;
+nome=prompt("Digite seu nome");//
+
+document.write("Nome digitado foi: "+nome);
