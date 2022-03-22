f59ffbbd5c7dba2922fc8a08e228835c354bce71
Hebert <hebertphp@gmail.com>
2022-03-21 22:07:49 -0300

    PFE_2a_M

--- aula3/script3_06-1.js
+++ aula3/script3_06-1.js
@@ -1,7 +1,13 @@
 var nome,ano,res;
 nome=prompt("Digite seu nome");//
+
 ano=prompt("Digite o ano atual");// "2022"
-ano=parseInt(ano);
-res=ano+2;//20222
+ano=parseInt(ano);//2022
+
+res=ano+2;//2022
 document.write("Nome digitado foi: "+nome);
 document.write("<br>Ano: "+res);
+/*
+parseInt - para numero inteiro 182
+parseFloat - para numero fracionado(float) 9.8
+*/
---
+++ aula3/script3_06-2.js
@@ -0,0 +1,16 @@
+var x,y,res;
+x=prompt("Digite um número");//7.5
+x=parseFloat(x);//7.5
+y=prompt("Digite outro número");//4.3
+y=parseInt(y);//4
+res=x-y;//res=7.5-4 res=3.5
+document.write("res = "+res);//
+/*
+
+idade int
+cpf int
+NNN.NNN.NN8-NN
+cep string NNNNN-NNN
+
+
+*/
