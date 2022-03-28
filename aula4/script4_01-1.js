f7800d508ecf0a4c94160bfb6afd2ce51984be70
Hebert <hebertphp@gmail.com>
2022-03-28 20:52:42 -0300

    PFE_2a_N

--- aula4/script4_01-1.js
+++ aula4/script4_01-1.js
@@ -1,9 +1,9 @@
 var x,y,z;
-y=prompt("Digite um número");
-y=parseInt(y);
-z=prompt("Digite outro número");//4
-z=parseInt(z);
+y=prompt("Digite um número");//"5"
+y=parseInt(y);//5
+z=prompt("Digite outro número");//"4"
+z=parseInt(z);//4
 
-x=2*y+z;//
+x=y+z;//x=5+4   x= 9
 
 document.write("x = "+x);//
---
+++ aula4/script4_02-1.js
@@ -0,0 +1,26 @@
+a=prompt("Digite um número");
+a=parseInt(a);
+b=prompt("Digite outro número");
+b=parseInt(b);
+document.write ("soma: "+ (a+b));
+document.write ("<br>subtração: "+ (a-b));
+
+/*
+19 % 2 = 1
+% - resto da divisão
+todo resto da div de número par,  vale 0
+1980 % 2 = 0
+todo resto da div de número ímpar, vale 1
+36436423649847 % 2 = 1
+
+157 % 3 = 2
+
+
+2022 inteiro par
+2021 inteiro ímpar
+
+9.6 fracionado(float)
+
+%2  (0,1)
+%3  (0,1,2)
+*/
