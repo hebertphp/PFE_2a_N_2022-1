2f31e47fcfe9d2e0cffb27762d7dccdd2b2e6b3d
Hebert <hebertphp@gmail.com>
2022-03-28 21:17:37 -0300

    PFE_2a_N

--- aula4/script4_02-1.js
+++ aula4/script4_02-1.js
@@ -1,11 +1,20 @@
-a=prompt("Digite um número");
+var a,b;
+a=prompt("Digite um número");//5
 a=parseInt(a);
-b=prompt("Digite outro número");
+b=prompt("Digite outro número");//3
 b=parseInt(b);
-document.write ("soma: "+ (a+b));
+document.write ("soma: "+(a+b));
 document.write ("<br>subtração: "+ (a-b));
+document.write ("<br>multiplicação: "+ (a*b));
+document.write ("<br>Divisão: "+(a/b)   );
+document.write ("<br>Módulo: "+(a%b)   );
+document.write ("<br>Exponenciação: "+(a**b)   );
 
 /*
+Divisão /
+Resto %
+Potencia **
+
 19 % 2 = 1
 % - resto da divisão
 todo resto da div de número par,  vale 0
