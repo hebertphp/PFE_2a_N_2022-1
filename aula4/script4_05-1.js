bba216aa90b7919424c4c996c042016339faf216
Hebert <hebertphp@gmail.com>
2022-04-04 20:56:55 -0300

    PFE_2a_N

---
+++ aula4/script4_05-1.js
@@ -0,0 +1,13 @@
+var a;
+a=prompt("Digite um número");//"2022"
+a=parseInt(a);//2022
+document.write ("a ="+ (a--));//a=2022
+document.write ("<br>a ="+ a);//a=2021
+/*
+++, incremento = somar 1
+--, decremento = subtrair 1
+++a     pré-incremento
+a++     pós-incremento
+--a     pré-decremento
+a--     pós-decremento
+*/
