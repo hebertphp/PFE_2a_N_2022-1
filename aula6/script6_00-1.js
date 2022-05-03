aec745d474c3d928be595bb751ab6210ab23d99c
Hebert <hebertphp@gmail.com>
2022-05-02 22:08:56 -0300

    PFE_2a_N

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -12,5 +12,7 @@ k=[10,50,2022,6,55,17];//array
 
 //document.getElementById("dv1").innerHTML="O valor de x vale:"+x;
 document.querySelector("#dv1").innerHTML="O valor de x vale:"+x;
-
+document.querySelector("#dv1").innerHTML="<h1>Teste de h1</h1>";
 document.querySelector(".teste").innerHTML=k;
+
+document.querySelector("h2").innerHTML+=", Mudou!!";
