8b6854a3a1013d30bf294a8262973a982d3c5550
Hebert <hebertphp@gmail.com>
2022-05-02 21:54:46 -0300

    PFE_2a_N

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -5,7 +5,12 @@ x=2022;//2022
 x=6;//6
 x=55;//55
 x=17;//
-document.write(x);//17
+//document.write(x);//17
 
 k=[10,50,2022,6,55,17];//array
-document.write("<br>"+k);
+//document.write("<br>"+k);
+
+//document.getElementById("dv1").innerHTML="O valor de x vale:"+x;
+document.querySelector("#dv1").innerHTML="O valor de x vale:"+x;
+
+document.querySelector(".teste").innerHTML=k;
