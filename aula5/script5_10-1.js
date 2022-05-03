7667d9fa66231562d4db8778423baa9ebaae4b35
Hebert <hebertphp@gmail.com>
2022-05-02 21:37:05 -0300

    PFE_2a_N

--- aula5/script5_10-1.js
+++ aula5/script5_10-1.js
@@ -5,6 +5,6 @@ for (var i=0; i<=6; i+=2) {
     document.write(i + " ");
 }
 /*
-0 1 2 3 4 5
+0 2 4 6
 
 */
---
+++ aula5/script5_11-1.js
@@ -0,0 +1,28 @@
+var i,j,k;
+for (i=0; i<3; i++) {//ext //hora
+    for (j=0; j<4; j++) {//int //minuto
+        for(k=0; k<2; k++) {//int //segundos
+           document.write(i+" "+j+" "+k+"<br>"); 
+        }
+        
+    }
+}
+/*
+Saída:
+i   j   k
+0   0
+0   1
+0   2
+0   3
+1   0
+1   1
+1   2   
+1   3
+2   0
+2   1
+2   2
+2   3
+
+
+
+*/
