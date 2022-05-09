6fef96f5b03d8345ed2a70b9f5eb5e1a0f985bd3
Hebert <hebertphp@gmail.com>
2022-05-09 20:58:37 -0300

    PFE_2a_N

---
+++ aula6/script6_01-1.js
@@ -0,0 +1,6 @@
+//             0    1    2
+var valores = ["SP",7.5,2021];
+nome=["João","Abelardo","Belarmina","Astrogildo"];
+document.querySelector("#dv1").innerHTML="Os valores são:<br>"+valores;
+document.querySelector(".teste").innerHTML=valores[1];
+document.querySelector("h2").innerHTML=nome[2];
