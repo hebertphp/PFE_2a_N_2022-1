abed0c0ae80a1eeceef0e85639995d7ce0c98840
Hebert <hebertphp@gmail.com>
2022-05-09 20:41:37 -0300

    PFE_2a_N

---
+++ aula6/script6_00-2.js
@@ -0,0 +1,9 @@
+var n,nome;
+n="João";
+n="Abelardo";
+n="Belarmina";
+n="Astrogildo";
+nome=["João","Abelardo","Belarmina","Astrogildo"];
+// document.write(nome);
+document.querySelector("#dv1").innerHTML=nome;
+document.querySelector(".teste").innerHTML=n;
