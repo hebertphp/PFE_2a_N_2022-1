a8abbf0365403e9678b3cfb98a65e23877302e7e
Hebert <hebertphp@gmail.com>
2022-05-16 20:52:13 -0300

    PFE_2a_N

---
+++ aula6/script6_06-1.js
@@ -0,0 +1,8 @@
+var uf=["SP","RJ","ES","MG","SC","RR","PR","TO","MS","AM","RN","CE"];
+var i,j;
+for(i=0;i<uf.length;i++){
+    document.querySelector("#dv1").innerHTML+=uf[i]+"|";
+}
+for(j=50;j<71;j++){
+    document.querySelector(".teste").innerHTML+=j+" ";
+}
---
+++ aula6/script6_06-2.js
@@ -0,0 +1,5 @@
+/*
+Criar um looping de:9193 até:9210
+A saída mostrasse os emojis dos valores da faixa acima
+link: https://www.w3schools.com/charsets/ref_emoji.asp
+*/
