aaa87bb3b4557db95a8900eea0d6d0516de7143b
Hebert <hebertphp@gmail.com>
2022-05-23 20:36:51 -0300

    PFE_2a_N

--- aula6/duvidas.js
+++ aula6/duvidas.js
@@ -1,3 +1,10 @@
 var uf=["SP","RJ","MG","ES"];
-document.querySelector("#nome").innerHTML=uf[1];
+var txt="Hoje tem atividade!";
+var class_teste=document.querySelectorAll(".teste");
+document.querySelector("#nome").innerHTML=uf[1]+" - "+uf[3];
 document.querySelector("h2").innerHTML+="- " +uf.length ;
+// document.querySelector(".teste").innerHTML=txt;
+// class_teste[3].innerHTML=txt;
+for(var i=0;i<class_teste.length;i++){
+    class_teste[i].innerHTML=txt+" "+i;
+}
