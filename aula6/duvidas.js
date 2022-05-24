8bd6b145b0e577e689b3eeacb1a8024a8400377f
Hebert <hebertphp@gmail.com>
2022-05-23 21:02:06 -0300

    PFE_2a_N

--- aula6/duvidas.js
+++ aula6/duvidas.js
@@ -4,7 +4,12 @@ var class_teste=document.querySelectorAll(".teste");
 document.querySelector("#nome").innerHTML=uf[1]+" - "+uf[3];
 document.querySelector("h2").innerHTML+="- " +uf.length ;
 // document.querySelector(".teste").innerHTML=txt;
+// class_teste[4].innerHTML=txt;
+// class_teste[0].innerHTML=txt;
 // class_teste[3].innerHTML=txt;
+
 for(var i=0;i<class_teste.length;i++){
-    class_teste[i].innerHTML=txt+" "+i;
+     class_teste[i].innerHTML=txt+" "+i;
 }
+var imagem=["comp.jpg","floresta.jpg"];
+document.querySelector(".img").innerHTML="<img src='img/comp.jpg'>";
