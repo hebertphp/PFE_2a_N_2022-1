cc6975527f607e4aa91e5cf3e3edf6e3e5d84f9b
Hebert <hebertphp@gmail.com>
2022-06-06 20:10:12 -0300

    PFE_2a_N

---
+++ aula6/revisao_1.js
@@ -0,0 +1,8 @@
+//     0  1  2   3  4
+var x=[10,20,30,40,50];
+//       0      1      2
+var y=[[3,4],[5,6,7],[8,9]];
+//      0 1   0 1 2   0 1
+
+document.querySelector("#dv").innerHTML=x.length;
+document.querySelector(".teste").innerHTML=y[2][1];
