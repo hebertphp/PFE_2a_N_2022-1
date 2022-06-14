74041c5e771eb85d3176fb4e192ef6f7301cc6c3
Hebert <hebertphp@gmail.com>
2022-06-13 21:40:05 -0300

    PFE_2a_N

--- aula7/calculadora.js
+++ aula7/calculadora.js
@@ -1,3 +1,26 @@
 function mostra(){
-    alert("Funcionou!");
+   const id_n1=document.querySelector("#n1");
+   const id_n2=document.querySelector("#n2");
+   const id_op=document.querySelector("#op");
+   document.querySelector("#saida").innerHTML=id_n1.value+"<br>"+id_op.value+"<br>"+id_n2.value;
+}
+function calc(n1,n2,op){
+    if(op=="+"){
+        return n1+n2;
+    }
+    else if(op=="-"){
+        return n1-n2;
+    }    
+    else if(op=="*"){
+        return n1*n2;
+    } 
+    else if(op=="/"){
+        return n1/n2;
+    } 
+    else if(op=="%"){
+        return n1%n2;
+    } 
+    else{
+        return "Operador desconhecido!";
+    }
 }
